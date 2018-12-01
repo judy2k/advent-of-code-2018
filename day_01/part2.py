fa7a920ba1388e63e1fbdee2c04dcbee7e3a7245
Mark Smith <mark.smith@practicalpoetry.co.uk>
2018-12-01 13:53:48 +0000

    Minor refactor.

--- day_01/part2.py
+++ day_01/part2.py
@@ -4,16 +4,19 @@ from itertools import accumulate, chain, cycle
 
 
 def changes(path):
-    return cycle(int(line) for line in open(path, encoding='utf-8'))
+    return cycle(int(line) for line in open(path, encoding="utf-8"))
 
 
 def frequencies(change_iter):
     return accumulate(chain([0], change_iter))
 
 
-seen_frequencies = set()
-for frequency in frequencies(changes('input.txt')):
-    if frequency in seen_frequencies:
-        print(frequency)
-        break
-    seen_frequencies.add(frequency)
+def first_seen_frequency(path):
+    seen_frequencies = set()
+    for frequency in frequencies(changes(path)):
+        if frequency in seen_frequencies:
+            return frequency
+        seen_frequencies.add(frequency)
+
+
+print(first_seen_frequency("input.txt"))
