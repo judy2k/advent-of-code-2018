a9413d6a997e45baea35b812e61f266767217685
Mark Smith <mark.smith@practicalpoetry.co.uk>
2018-12-01 19:13:18 +0000

    Solve the problems in Rust.

--- day_01/rust/src/bin/part1.rs
+++ day_01/rust/src/bin/part1.rs
@@ -1,3 +1,15 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
+
 fn main() {
-    println!("Hello, part1!");
+    let f = File::open("../input.txt").unwrap();
+    println!(
+        "{}",
+        BufReader::new(f)
+            .lines()
+            .map(Result::unwrap)
+            .map(|s| s.parse::<i32>().unwrap())
+            .sum::<i32>()
+    );
 }
--- day_01/rust/src/bin/part2.rs
+++ day_01/rust/src/bin/part2.rs
@@ -1,3 +1,48 @@
+use std::collections::HashSet;
+use std::fs::File;
+use std::io;
+use std::io::prelude::*;
+
 fn main() {
-    println!("Hello, part2!");
+    let mut seen_vals = HashSet::new();
+    for frequency in Frequencies::new(changes("../input.txt").expect("error")) {
+        if seen_vals.contains(&frequency) {
+            println!("{}", frequency);
+            break;
+        }
+        seen_vals.insert(frequency);
+    }
+}
+
+struct Frequencies {
+    current: i32,
+    changes: Vec<i32>,
+    next_change: usize,
+}
+
+impl Frequencies {
+    fn new(changes: Vec<i32>) -> Frequencies {
+        Frequencies {
+            current: 0, changes, next_change: 0
+        }
+    }
+}
+
+impl Iterator for Frequencies {
+    type Item = i32;
+    fn next(&mut self) -> Option<i32> {
+        let result = self.current;
+        self.current += self.changes[self.next_change];
+        self.next_change  = (self.next_change + 1) % self.changes.len();
+        Some(result)
+    }
+}
+
+fn changes(path: &str) -> Result<Vec<i32>, io::Error> {
+    let f = File::open(path)?;
+    Ok(io::BufReader::new(f)
+        .lines()
+        .map(Result::unwrap)
+        .map(|s| s.parse::<i32>().unwrap())
+        .collect::<Vec<i32>>())
 }
