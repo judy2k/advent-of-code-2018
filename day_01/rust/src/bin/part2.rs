01a62b2baa6056d60fa5bbadb6f99cdc7ce5e0a6
Mark Smith <mark.smith@practicalpoetry.co.uk>
2018-12-01 22:33:02 +0000

    Better error handling.

--- day_01/rust/src/bin/part2.rs
+++ day_01/rust/src/bin/part2.rs
@@ -6,17 +6,24 @@ use std::io::prelude::*;
 fn main() {
     let mut current_val = 0;
     let mut seen_vals = HashSet::new();
-    for change in changes("../input.txt").expect("error").iter().cycle() {
-        if seen_vals.contains(&current_val) {
-            println!("{}", current_val);
-            break;
-        }
-        seen_vals.insert(current_val);
-        current_val += change;
+    match int_file("../input.txt") {
+        Ok(is) => {
+            for change in is.iter().cycle() {
+                if seen_vals.contains(&current_val) {
+                    println!("{}", current_val);
+                    break;
+                }
+                seen_vals.insert(current_val);
+                current_val += change;
+            }
+        },
+        Err(e) => {
+            println!("Error: {}", e);
+        },
     }
 }
 
-fn changes(path: &str) -> Result<Vec<i32>, io::Error> {
+fn int_file(path: &str) -> Result<Vec<i32>, io::Error> {
     let f = File::open(path)?;
     Ok(io::BufReader::new(f)
         .lines()
