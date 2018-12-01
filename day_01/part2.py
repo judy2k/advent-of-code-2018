3e0c701079fcc2b83292233c290e0be519b95a8c
Mark Smith <mark.smith@practicalpoetry.co.uk>
2018-12-01 14:03:01 +0000

    Not more readable, but shorter.

--- day_01/part2.py
+++ day_01/part2.py
@@ -3,17 +3,11 @@
 from itertools import accumulate, chain, cycle
 
 
-def changes(path):
-    return cycle(int(line) for line in open(path, encoding="utf-8"))
-
-
-def frequencies(change_iter):
-    return accumulate(chain([0], change_iter))
-
-
 def first_seen_frequency(path):
     seen_frequencies = set()
-    for frequency in frequencies(changes(path)):
+    for frequency in accumulate(
+        chain([0], cycle(int(line) for line in open(path, encoding="utf-8")))
+    ):
         if frequency in seen_frequencies:
             return frequency
         seen_frequencies.add(frequency)
