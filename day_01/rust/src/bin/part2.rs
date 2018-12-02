9754911bab0a148cd4577efe502694d845f3add6
Mark Smith <mark.smith@practicalpoetry.co.uk>
2018-12-02 12:44:52 +0000

    Refactoring and tests.

--- day_01/rust/src/bin/part2.rs
+++ day_01/rust/src/bin/part2.rs
@@ -21,9 +21,14 @@ fn int_file(path: &str) -> Result<Vec<i32>, io::Error> {
 }
 
 
+fn frequencies(v: Vec<i32>) -> impl Iterator<Item = i32> {
+    vec!(0 as i32).into_iter().chain(v.into_iter().cycle()).aggregate()
+}
+
+
 fn calculate_result(path: &str) -> Result<i32, io::Error> {
     let mut seen_vals = HashSet::new();
-    for frequency in vec!(0 as i32).into_iter().chain(int_file(path)?.into_iter().cycle()).aggregate() {
+    for frequency in frequencies(int_file(path)?) {
         if seen_vals.contains(&frequency) {
             return Ok(frequency);
         }
@@ -37,4 +42,9 @@ fn calculate_result(path: &str) -> Result<i32, io::Error> {
 fn test_calculate_result() {
     assert_eq!(calculate_result("../sample_input.txt").unwrap(), 2);
     assert_eq!(calculate_result("../input.txt").unwrap(), 55250);
+}
+
+#[test]
+fn test_frequencies() {
+    assert_eq!(frequencies(vec!(4, -5, 6)).take(6).collect::<Vec<i32>>(), vec!(0, 4, -1, 5, 9, 4));
 }
